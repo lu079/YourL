44cf773c4b73ad17c34fa7f9c279c3b4d7702039
Lourdes Catalán <95139998+lu079@users.noreply.github.com>
2022-02-25 13:29:38 -0600

    Update serializer.py

--- Apis_cargamos/Apis_cargamos/app/serializer.py
+++ Apis_cargamos/Apis_cargamos/app/serializer.py
@@ -15,30 +15,33 @@ productCategorys_schema=ProductCategorySerializer(many=True)
 class ProductSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model=Product
-        fields=('id','name','presentation','description','posology','management','stock_code','category_id','price','discount_price')
+        fields=('id','name','presentation','description','posology','management','stock_code','category_id','category','price','discount_price')
 product_schema=ProductSerializer()
 products_schema=ProductSerializer(many=True)
 
 class OrderSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model=Order
-        fields=('id','user_id','order_num','delivery_date')
+        fields=('id','user_id','user','order_num','delivery_date')
 order_schema=OrderSerializer()
 
 class OrderProductSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model=OrderProduct
-        fields=('id','order_id','product_id','quantity')
+        fields=('id','order_id','order','product_id','product','quantity')
 orderProduct_schema=OrderSerializer()
 
 class UserAddressSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model=UserAddress
-        fields=('id','user_id','address1','address2','zipcode','country','city','phone')
+        fields=('id','user_id','user','address1','address2','zipcode','country','city','phone')
 userAddress_schema=OrderSerializer()
 
 class PaymentSerializer(ma.SQLAlchemyAutoSchema):
     class Meta:
         model=Payment
-        fields=('id','order_id','paymant_date','payment_type')
+        fields=('id','order_id','order','paymant_date','payment_type')
 payment_schema=OrderSerializer()
+
+
+
