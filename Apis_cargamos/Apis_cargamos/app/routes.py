cdd3d8b6552b9bfcbcaea50fb481abb32c7f64c5
Lourdes Catalán <95139998+lu079@users.noreply.github.com>
2022-02-24 22:11:45 -0600

    Update routes.py

--- Apis_cargamos/Apis_cargamos/app/routes.py
+++ Apis_cargamos/Apis_cargamos/app/routes.py
@@ -1,22 +1,23 @@
-
-from flask import Flask,redirect,request,flash,make_response,jsonify
+from flask import redirect, render_template,request,flash,url_for,make_response,jsonify
 from app import app,db
-from app.models import users,product_category,products
+from app.models import ProductCategory,Product,Order,OrderProduct,UserAddress,Payment
+from app.serializer import productCategory_schema,productCategorys_schema,product_schema,products_schema,order_schema,orderProduct_schema,userAddress_schema,payment_schema
+
 from flask_cors import cross_origin
 
 @cross_origin
 @app.route("/listar_producto",methods=["GET"])
 def listar_producto():
     #todo seleccionado todos los objetos de la clase grupos
-    products=products.query.all()
+    products=Product.query.all()
     #todo serializando y seleccionado los atributos a cast en json
     #todo dump nos permite serializar los objetos de PYTHON
-    result=products.dump(products)
+    result=products_schema.dump(products)
     #todo creando el documento de salida
     data={
         'message':'Todos mis productos',
         'status':200,
-        'data':{}
+        'data':result
     }
     return make_response(jsonify(data))
 
